f704858049ae9ee24d31011efbf1126a802225d4
Corbin Taylor <cptaylor38@yahoo.com>
2019-03-09 21:19:00 -0500

    removed api keys and excess comments

--- assets/javascript2c.js
+++ assets/javascript2c.js
@@ -1,17 +1,3 @@
-// var config = {
-//     apiKey: "AIzaSyC6esOI7MQQZKAI4G0jllI2WQUiqVeT-5M",
-//     authDomain: "project1-443df.firebaseapp.com",
-//     databaseURL: "https://project1-443df.firebaseio.com",
-//     projectId: "project1-443df",
-//     storageBucket: "project1-443df.appspot.com",
-//     messagingSenderId: "760943696966"
-// };
-
-// firebase.initializeApp(config);
-
-// var database = firebase.database();
-
-
 var $wordInput = $('#wordInput');
 var $dictionarySubmit = $('#dictionarySubmit');
 
